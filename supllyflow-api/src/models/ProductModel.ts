f9c4c1e0f035dc25378d4ebcf2d7239c6819fee7
Daricrys F. Belém <112405945+Daricrys@users.noreply.github.com>
2023-10-08 13:55:01 -0300

    add ProductModel

---
+++ supllyflow-api/src/models/ProductModel.ts
@@ -0,0 +1,10 @@
+export type ProductModel = {
+    id: string
+    name: string;
+    description: String;
+    bar_code:    String;
+    amount:     String;
+    category:    String;
+    supplierId: String;
+
+}
