96aaf26fc4ea591cdb7120de3a2b525602324a3b
TagoreMatheus <144496597+TagoreMatheus@users.noreply.github.com>
2023-10-08 21:47:18 -0300

    add signup3

---
+++ supllyflow-app/src/pages/signUp/signUp3/style.ts
@@ -0,0 +1,35 @@
+import { StyleSheet } from 'react-native';
+import { THEME } from '../../../theme/theme';
+
+export const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    page: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+    },
+    container: {
+        marginTop: '20%',
+        width: '80%',
+    },
+    title: {
+        color: THEME.COLORS.PRIMARY,
+        fontSize: 36,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginBottom: 20
+    },
+    input: {
+        backgroundColor: THEME.COLORS.BACKGROUND_800,
+        color: THEME.COLORS.TEXT,
+        fontSize: 18,
+        height: 48,
+        borderRadius: 8,
+        paddingLeft: 20,
+        marginTop: 20,
+        fontWeight: '500'
+    },
+})
