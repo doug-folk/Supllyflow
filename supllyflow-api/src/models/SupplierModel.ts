58fe7aa2552ccfdca1b55e65dd6a8ab39515ebbb
doug-folk <dougf3lipe@gmail.com>
2023-10-16 19:01:48 -0300

    Fix: Controller supllier

--- supllyflow-api/src/models/SupplierModel.ts
+++ supllyflow-api/src/models/SupplierModel.ts
@@ -1,10 +1,8 @@
 export type SupplierModel = {
     id: string;
   name: string;
-  contact: String;
-  description: String;
-  category: String;
-  user: User;
-  userId: String;
-  products: Product[];
+  contact: string;
+  description: string;
+  category: string;
+  userId: string;
 }
