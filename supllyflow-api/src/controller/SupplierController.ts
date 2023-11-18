1998ec576318a4f728f62b9b097d1e72c62caaa7
livia araujo <liviaaraujo.dev@gmail.com>
2023-11-18 11:53:25 -0300

    update supplier api

--- supllyflow-api/src/controller/SupplierController.ts
+++ supllyflow-api/src/controller/SupplierController.ts
@@ -2,32 +2,35 @@ import { Request, Response } from "express";
 import prisma from "../utils/prisma";
 import { SupplierModel } from "../models/SupplierModel";
 import { getToken } from "../utils/token";
+import { SupplierRepository } from "../repositories/SupplierRepository";
 
 export class SupplierController{
     async create(req: Request, res: Response) {   
         const userId = getToken(req, res);
 
-        const { name,reasonSocial, cnpj, email, telephone } : SupplierModel = req.body;
-        const supplier =  await prisma.supplier.create({
-            data: {
-                name,
-                userId: userId,
-                cnpj,
-                reasonSocial,
-                email,
-                telephone
-            }
-        })
-
-        return res.json({ supplier });    
+        const data: SupplierModel = req.body;
+        
+        const result = await SupplierRepository.create(data, userId);  
+        
+        return res.json({result});   
     }
 
     async findSuppliersUser(req: Request, res: Response) {  
         const userId = getToken(req, res);
 
-        const suppliers = await prisma.supplier.findMany({
-            where: { userId: userId },
-         });
+        const suppliers = await SupplierRepository.findSuppliersUser(userId);
+
         return res.json({suppliers})
     }
+
+    async updateSupplier(req: Request, res: Response) {   
+        // const userId = getToken(req, res);
+
+        const data: SupplierModel = req.body;
+        
+        const result = await SupplierRepository.updateSupplier(data);  
+        
+        return res.json({result});   
+    }
+
 }
---
+++ supllyflow-api/src/repositories/SupplierRepository.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from "express";
+import prisma from "../utils/prisma";
+import { SupplierModel } from "../models/SupplierModel";
+import { getToken } from "../utils/token";
+
+export class SupplierRepository{
+    static async create(data: SupplierModel, userId: string) {   
+
+        const supplier =  await prisma.supplier.create({
+            data: {
+                name: data.name,
+                userId: userId,
+                cnpj:data.cnpj,
+                reasonSocial: data.reasonSocial,
+                email: data.email,
+                telephone: data.telephone
+            }
+        })
+
+        return supplier;    
+    }
+
+    static async findSuppliersUser(userId: string) {  
+
+        const suppliers = await prisma.supplier.findMany({
+            where: { userId: userId },
+         });
+        return suppliers;
+    }
+
+    static async updateSupplier(data: SupplierModel ) {
+        const supplier = prisma.supplier.update({
+            where: { id: data.id},
+            data: data
+        });
+
+        return supplier;
+    }
+}
