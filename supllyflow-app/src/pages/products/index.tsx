a632e86905bcb1ba706a33b5b7d0e5a69d3c7c84
livia araujo <liviaaraujo.dev@gmail.com>
2023-11-19 09:59:00 -0300

    corrections app

--- supllyflow-app/src/pages/products/index.tsx
+++ supllyflow-app/src/pages/products/index.tsx
@@ -5,6 +5,7 @@ import {
   TouchableOpacity,
   SafeAreaView,
   FlatList,
+  ActivityIndicator,
 } from "react-native";
 import { useFocusEffect, useNavigation } from "@react-navigation/native";
 import { MaterialIcons } from "@expo/vector-icons";
@@ -13,6 +14,7 @@ import { api } from "../../services";
 import { useAuth } from "../../contexts/AuthContext";
 import { Product } from "../../utils/interfaces/product";
 import { Supplier } from "../../utils/interfaces/supplier";
+import { THEME } from "../../theme/theme";
 
 type Navigation = {
   navigate: (value: string, {}?: Product) => void;
@@ -23,11 +25,10 @@ export function Products() {
   const navigation = useNavigation<Navigation>();
 
   const [products, setProducts] = useState<Product[]>([]);
+  const [isCarreg, setIsCarreg] = useState<boolean>(false);
 
   const { getToken } = useAuth();
   const token = getToken();
-
-  
 
   async function getProducts() {
     await api
@@ -38,6 +39,7 @@ export function Products() {
       })
       .then((response) => {
         setProducts(response.data.products);
+        setIsCarreg(true);
       })
       .catch((error) => {
         console.error(error);
@@ -52,43 +54,60 @@ export function Products() {
     navigation.navigate("detailsProduct", product);
   }
 
-  return (
-    <SafeAreaView style={styles.body}>
-      <View style={styles.circleValue}>
-        <Text style={styles.circleValueText}>R$ 50,00</Text>
-      </View>
+  if (isCarreg) {
+    return products.length > 0 ? (
+      <SafeAreaView style={styles.body}>
+        <View style={styles.circleValue}>
+          <Text style={styles.circleValueText}>R$ 50,00</Text>
+        </View>
 
-      <View style={styles.subtitleRow}>
-        <Text style={styles.subtitle}>Produtos</Text>
-      </View>
+        <View style={styles.subtitleRow}>
+          <Text style={styles.subtitle}>Produtos</Text>
+        </View>
 
-      <FlatList
-        data={products}
-        style={{ width: "90%" }}
-        renderItem={({ item, index }) => (
-          <TouchableOpacity style={styles.products} onPress={() => clickProduct(item)}>
-            <View style={styles.productsRow}>
-              <View style={styles.productsRowLeft}>
-                <View style={styles.circle} />
-                <Text style={styles.productsName}>{item.name}</Text>
+        <FlatList
+          data={products}
+          style={{ width: "90%" }}
+          renderItem={({ item, index }) => (
+            <TouchableOpacity style={styles.products} onPress={() => clickProduct(item)}>
+              <View style={styles.productsRow}>
+                <View style={styles.productsRowLeft}>
+                  <View style={styles.circle} />
+                  <Text style={styles.productsName}>{item.name}</Text>
+                </View>
+                <View>
+                  <Text style={styles.productsName}>R$ {item.amount}</Text>
+                </View>
               </View>
-              <View>
-                <Text style={styles.productsName}>R$ {item.amount}</Text>
-              </View>
-            </View>
-            <View style={styles.line} />
-          </TouchableOpacity>
-        )}
-      />
+              <View style={styles.line} />
+            </TouchableOpacity>
+          )}
+        />
 
-      <TouchableOpacity
-        style={styles.floatingButton}
-        onPress={() => {
-          navigation.navigate("createProduct" as never);
-        }}
-      >
-        <MaterialIcons name="add" size={28} color="#FFFFFF" />
-      </TouchableOpacity>
-    </SafeAreaView>
-  );
+        <TouchableOpacity
+          style={styles.floatingButton}
+          onPress={() => {
+            navigation.navigate("createProduct" as never);
+          }}
+        >
+          <MaterialIcons name="add" size={28} color="#FFFFFF" />
+        </TouchableOpacity>
+      </SafeAreaView>
+      
+    ) : (
+      <View>
+        <Text>Teste</Text>
+      </View>
+    );
+  } else {
+    return (
+      <View style={styles.load}>
+        <ActivityIndicator
+          size="200"
+          color={THEME.COLORS.PRIMARY}
+          style={{ alignItems: "center", justifyContent: "center" }}
+        />
+      </View>
+    );
+  }
 }
